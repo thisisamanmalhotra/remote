2b6b7d22e47010ee30b430cf189fd03172fa0b41
aman malhotra <amanmalhotraa5@gmail.com>
2022-12-14 21:22:38 +0100

    main push

---
+++ Indifin-main 2/public/build/assets/LayoutInstallation.8be2bcde.js
@@ -0,0 +1,1 @@
+import{N as t}from"./NotificationRoot.5f448391.js";import{r as s,o as r,e as a,f as o,h as c}from"./vendor.4cee486b.js";import"./main.3742ff98.js";const n={class:"h-screen overflow-y-auto text-base"},i={class:"container mx-auto px-4"},u={setup(_){return(f,m)=>{const e=s("router-view");return r(),a("div",n,[o(t),c("div",i,[o(e)])])}}};export{u as default};
---
+++ Indifin-main 2/readme.md
@@ -0,0 +1,7 @@
+# Indifin
+
+## Introduction
+
+Indifin is an web & mobile app that helps you track expenses, payments & create professional invoices & estimates.
+
+Web Application is made using Laravel & VueJS
